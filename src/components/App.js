70ebd83f1f3fe98a65c6a2641316cec6599a0199
Shardul Kurdukar <24shardul@gmail.com>
2022-03-28 18:42:30 +0530

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const clickHandler = () => {
-    setCount(prevCount =>  (prevCount+ 1)*(prevCount+1));
-  }
+  const clickHandler = () => {};
   return (
     <div>
       <h3>Current Count:</h3>
-      <h2 id="count">{count}</h2>
-      <p>Count should be initially set to 0, and increment as per the function (prevCount+ 1)*(prevCount+1)</p>
+      {/* Add a h2 tag here with id as "count " and inner text as count variable */}
+
+      <p>
+        Count should be initially set to 0, and increment as per the function
+        (prevCount+ 1)*(prevCount+1)
+      </p>
       <button id="btn" onClick={clickHandler}>
         Click me to increment
       </button>
     </div>
   );
-}
+};
 
 export default App;
